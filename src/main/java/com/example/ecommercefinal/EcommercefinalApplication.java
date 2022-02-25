a4cc9a958097c6f72da8039ea5d87108467067ed
uradatafarm <ura@datafarm.co.th>
2022-02-25 18:38:04 +0700

    First commit for Spring Boot project

---
+++ src/main/java/com/example/ecommercefinal/EcommercefinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercefinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercefinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommercefinal/EcommercefinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercefinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
