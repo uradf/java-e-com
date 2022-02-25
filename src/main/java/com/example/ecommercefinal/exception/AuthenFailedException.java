42d841fe4068a41a403897593376e4b164249973
uradatafarm <ura@datafarm.co.th>
2022-02-26 03:33:57 +0700

    [ECOM-02] Add Customer related exceptions

---
+++ src/main/java/com/example/ecommercefinal/exception/AuthenFailedException.java
@@ -0,0 +1,8 @@
+package com.example.ecommercefinal.exception;
+
+public class AuthenFailedException extends  RuntimeException{
+
+    public AuthenFailedException(String s) {
+        super();
+    }
+}
