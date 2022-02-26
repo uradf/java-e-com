80222aca89bfe327da47b2dc6a5ca17772e611a6
uradf <99809859+uradf@users.noreply.github.com>
2022-02-27 04:19:42 +0700

    Revert "Feature/ecom 03 add remove product to basket"

--- src/main/java/com/example/ecommercefinal/exception/AuthenFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommercefinal.exception;
-
-public class AuthenFailedException extends  RuntimeException{
-
-    public AuthenFailedException(String s) {
-        super();
-    }
-}
--- src/main/java/com/example/ecommercefinal/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommercefinal.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-    public CustomerNotFoundException() {
-        super();
-    }
-}
